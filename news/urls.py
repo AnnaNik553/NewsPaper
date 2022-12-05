e82d4cef23c0453f32c4582f7fb7fd50634e0121
Anna Nikiforyak <a.nikiforyak@gmail.com>
2022-12-05 15:58:22 +0300

    added pogonation and filters

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, SearchPostListView
 
 app_name = 'news'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='all_news'),
+    path('search/', SearchPostListView.as_view(), name='search'),
     path('<int:pk>/', PostDetailView.as_view(), name='detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Post
+from .filters import PostFilter
 
 # Create your views here.
 
@@ -11,6 +12,20 @@ class PostListView(ListView):
     template_name = 'news/posts.html'
     context_object_name = 'posts'
     ordering = '-created_at'
+    paginate_by = 10
+
+
+class SearchPostListView(ListView):
+    model = Post
+    template_name = 'news/search_posts.html'
+    context_object_name = 'posts'
+    ordering = '-created_at'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
 
 
 class PostDetailView(DetailView):
