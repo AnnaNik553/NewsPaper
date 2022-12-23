5450b16749e158ffa68d88300ecca68457183331
Anna Nikiforyak <a.nikiforyak@gmail.com>
2022-12-23 16:32:19 +0300

    admin panel is configured

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-from .models import Author, Post, Category, Comment
+from .models import Author, Post, Category, Comment, PostCategory
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating')
+    list_filter = ('user',)
+    search_fields = ('user',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'type_of_post', 'created_at', 'title', 'rating']
+    search_fields = ('author', 'title', 'rating')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Comment)
+admin.site.register(PostCategory)
+
+
+
+
